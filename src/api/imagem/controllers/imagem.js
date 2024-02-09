11ae00b4afb5589d69c9003fe8d805d9a62ae07d
caeg0n <caegon2005@hotmail.com>
2024-02-09 12:14:26 -0300

    update

--- src/api/imagem/controllers/imagem.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * imagem controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::imagem.imagem');
--- src/api/imagem/routes/imagem.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * imagem router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::imagem.imagem');
